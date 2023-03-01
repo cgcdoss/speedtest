6fcaf1203e2bbe1d3fb50fbf27962b9db84d3edc
cgcdoss <gabi04061997@gmail.com>
2023-03-01 00:06:42 -0300

    feat: adicionando barra de progresso

--- src/teste-velocidade.ts
+++ src/teste-velocidade.ts
@@ -2,23 +2,48 @@ export function setTeste(element: HTMLDivElement): any {
     let amostras: Array<number> = [];
     const TOTAL_AMOSTRAS = 15;
 
-    const btn = document.createElement('button');
-    btn.textContent = 'Começar';
-    btn.addEventListener('click', () => {
-        if (btn.textContent?.includes('Começar')) {
-            amostras = [];
-            setStatus('Carregando');
-            testarVelocidade()
-            btn.textContent = 'Parar';
-        } else {
-            btn.textContent = 'Começar';
-        }
-    });
-    element.append(btn);
+    let btn: HTMLButtonElement;
+    let status: HTMLHeadingElement;
+    let barraProgressoWrapper: HTMLDivElement;
+    let barraProgresso: HTMLDivElement;
 
-    const status = document.createElement('h2');
-    status.classList.add('status');
-    element.append(status);
+    function setBtnElement(): void {
+        btn = document.createElement('button');
+        btn.textContent = 'Começar';
+        btn.addEventListener('click', () => {
+            if (btn.textContent?.includes('Começar')) {
+                amostras = [];
+                setProgresso(0);
+                setStatus('Carregando');
+                testarVelocidade()
+                btn.textContent = 'Parar';
+            } else {
+                btn.textContent = 'Começar';
+            }
+        });
+        element.append(btn);
+    }
+
+    function setStatusElement(): void {
+        status = document.createElement('h2');
+        status.classList.add('status');
+        element.append(status);
+    }
+
+    function setBarraProgressoElement(): void {
+        barraProgressoWrapper = document.createElement('div');
+        barraProgresso = document.createElement('div');
+
+        barraProgressoWrapper.classList.add('barra-progresso-wrapper');
+        barraProgresso.classList.add('barra-progresso');
+
+        barraProgressoWrapper.append(barraProgresso);
+        element.append(barraProgressoWrapper);
+    }
+
+    function setProgresso(progresso: number): void {
+        barraProgresso.style.width = `${progresso}%`;
+    }
 
     function testarVelocidade() {
         const url = 'https://raw.githubusercontent.com/jrquick17/ng-speed-test/02c59e4afde67c35a5ba74014b91d44b33c0b3fe/demo/src/assets/5mb.jpg?nnn=0.6578724231971347';
@@ -38,12 +63,11 @@ export function setTeste(element: HTMLDivElement): any {
                 const fim = performance.now();
                 const tempo = (fim - inicio) / 1000; // converte de milissegundos para segundos
                 const velocidade = (buffer.byteLength / tempo / 1000000) * 8; // calcula a velocidade em Mbps
-                setStatus(`Velocidade de download: ${velocidade.toFixed(2)} Mbps`);
+                setStatus(`${velocidade.toFixed(2)} Mbps`);
                 amostras.push(+velocidade.toFixed(2));
+                setProgresso(amostras.length / TOTAL_AMOSTRAS * 100);
 
                 if (amostras.length === TOTAL_AMOSTRAS) {
-                    const media = amostras.reduce((prev, curr) => prev + curr) / TOTAL_AMOSTRAS;
-                    setStatus(`Velocidade média de ${media.toFixed(2)} Mbps`);
                     btn.textContent = 'Começar';
                     return;
                 }
@@ -59,5 +83,9 @@ export function setTeste(element: HTMLDivElement): any {
         return element.querySelector('.status')!.textContent = status;
     }
 
+    setBtnElement();
+    setStatusElement();
+    setBarraProgressoElement();
+    // @ts-ignore
     btn.click(); // Para iniciar o site já com o teste sendo executado
 }
